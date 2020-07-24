6423fc21f4848ea7eefd24f49faf7feb242ee582
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2020-07-24 19:33:32 +0300

    Ajout du perso

---
+++ Scripts/Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 80
+const ACCELERATION = 400
+const FRICTION = 400
+
+var velocity = Vector2.ZERO
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		#velocity += input_vector * ACCELERATION * delta
+		#velocity = velocity.clamped(MAX_SPEED * delta)
+	else :
+		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
+	velocity = move_and_slide(velocity)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
