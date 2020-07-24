2c811571dd8f8c7f5fb7ecb1269d7426f3e9f24c
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2020-07-24 21:02:59 +0300

    Animation Joueur Incomplete

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,11 +6,12 @@ const FRICTION = 400
 
 var velocity = Vector2.ZERO
 
+onready var anim_perso = $AnimPerso
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,7 +23,17 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	
+	if input_vector.x > 0 and input_vector.y == 0:
+		anim_perso.play("DeplDroite")
+	elif input_vector.x < 0 and input_vector.y == 0:
+		anim_perso.play("DeplGauche")
+
+	if input_vector.y > 0 and input_vector.x == 0:
+		anim_perso.play("DeplBas")
+	elif input_vector.y < 0 and input_vector.x == 0:
+		anim_perso.play("DeplHaut")
+		
+			
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		#velocity += input_vector * ACCELERATION * delta
