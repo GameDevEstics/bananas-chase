129fea7d95da607bd6da7fdaa05d35caea99c099
Nomena Christian <christian.nomena.razakamahery@esti.mg>
2020-07-24 22:23:46 +0300

    Mode multiplayer

---
+++ scripts/Player1.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 96
+const ACCELERATION = 400
+const FRICTION = 400
+
+# var speed = 96
+var velocity = Vector2.ZERO
+
+onready var anim_perso = $AnimPerso
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position.x = 36
+	position.y = 62
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("p1_right") - Input.get_action_strength("p1_left")
+	input_vector.y = Input.get_action_strength("p1_down") - Input.get_action_strength("p1_up")
+	
+	input_vector = input_vector.normalized()
+	
+	if input_vector.x > 0 and input_vector.y == 0:
+		anim_perso.play("DeplDroite")
+	elif input_vector.x < 0 and input_vector.y == 0:
+		anim_perso.play("DeplGauche")
+
+	if input_vector.y > 0 and input_vector.x == 0:
+		anim_perso.play("DeplBas")
+	elif input_vector.y < 0 and input_vector.x == 0:
+		anim_perso.play("DeplHaut")
+		
+			
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		#velocity += input_vector * ACCELERATION * delta
+		#velocity = velocity.clamped(MAX_SPEED * delta)
+	else :
+		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
+	velocity = move_and_slide(velocity)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- scripts/Player2.gd
+++ scripts/Player2.gd
@@ -1,28 +1,27 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 80
+const MAX_SPEED = 96
 const ACCELERATION = 400
 const FRICTION = 400
 
+# var speed = 96
 var velocity = Vector2.ZERO
 
 onready var anim_perso = $AnimPerso
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-	
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.x = 32
-	position.y = 64
+	position.x = 988
+	position.y = 568
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
+	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
 	input_vector = input_vector.normalized()
+	
 	if input_vector.x > 0 and input_vector.y == 0:
 		anim_perso.play("DeplDroite")
 	elif input_vector.x < 0 and input_vector.y == 0:
@@ -41,6 +40,7 @@ func _physics_process(delta):
 	else :
 		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
 	velocity = move_and_slide(velocity)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
