388d5e3a03353c8dc8e5794c5e99183e0a6afca7
Nomena Christian <christian.nomena.razakamahery@esti.mg>
2020-07-25 18:46:14 +0300

    Ajout des sons (Menu + Gameplay)

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -5,7 +5,13 @@ onready var scene_gameplay = $Gameplay
 onready var buttons = $Button
 onready var PlayButton = $Button/PlayButton
 
+onready var menu_sound = $MenuSound
+onready var gameplay_sound = $GameplaySound
+
 func _ready():
+	gameplay_sound.stop()
+	menu_sound.play()
+	
 	scene_menu.show()
 	buttons.show()
 	scene_gameplay.hide()
@@ -14,6 +20,9 @@ func _ready():
 #	pass
 
 func _on_PlayButton_pressed():
+	menu_sound.stop()
+	gameplay_sound.play()
+	
 	scene_menu.hide()
 	buttons.hide()
 	scene_gameplay.show()
